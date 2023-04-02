6c74ecb18c763dca780f9f7ec4047a1a50791662
SkyKeyUA <veremiienkoov96@gmail.com>
2023-04-02 21:36:07 +0300

    2 pages were added (authors / posts)

---
+++ pages/authors/index.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import Link from 'next/link';
+
+/** @format */
+export default function Authors() {
+  return (
+    <>
+      <h1>Authors</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
